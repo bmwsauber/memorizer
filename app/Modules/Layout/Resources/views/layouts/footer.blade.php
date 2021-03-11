72a965489d1c3f2fe0d242f0205156ee1c6494a8
Alex Du <alexandr.dubovis@gmail.com>
2021-03-11 11:54:29 +0200

    layout upds

--- app/Modules/Layout/Resources/views/layouts/footer.blade.php
+++ app/Modules/Layout/Resources/views/layouts/footer.blade.php
@@ -2,11 +2,9 @@
     <a @if (Route::currentRouteName() !== 'home') href="{{ route('home') }}" @endif>Home</a> |
     @if(session('current_report_id'))
         <a @if (Route::currentRouteName() !== 'work.repeat') href="{{ route('work.repeat', session('current_report_id')) }}" @endif>Repeat</a> |
-        <a @if (Route::currentRouteName() !== 'work.listening') href="{{ route('work.listening', session('current_report_id')) }}" @endif>Listening</a> |
         <a @if (Route::currentRouteName() !== 'work.idioms') href="{{ route('work.idioms', session('current_report_id')) }}" @endif>Idioms</a> |
     @else
         <a @if (Route::currentRouteName() !== 'work.repeat') href="{{ route('work.start', 'repeat') }}" @endif>Repeat</a> |
-        <a @if (Route::currentRouteName() !== 'work.listening') href="{{ route('work.start', 'listening') }}" @endif>Listening</a> |
         <a @if (Route::currentRouteName() !== 'work.idioms') href="{{ route('work.start', 'idioms') }}" @endif>Idioms</a> |
     @endif
     <a  @if (Route::currentRouteName() !== 'cards.learn') href="{{ route('cards.learn') }}" @endif>Learn New</a> |
--- app/Modules/Pages/Resources/views/home.blade.php
+++ app/Modules/Pages/Resources/views/home.blade.php
@@ -13,6 +13,5 @@
     ></last-report-data>
     <div class="buttons text-white mt-10 flex justify-center">
         <a href="{{ route('work.start', 'repeat') }}" class="bg-teal-100 hover:bg-teal-200 px-12 py-2 mx-3 rounded">Repeat</a>
-        <a href="{{ route('work.start', 'listening') }}" class="bg-teal-100 hover:bg-teal-200 px-12 py-2 mx-3 rounded">Listening</a>
     </div>
 @endsection
